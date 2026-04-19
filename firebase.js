0ea7cbc77a63db2f5c03192da2ac3f946486eef3
MAYANK <mayankmishra33275@gmail.com>
2026-04-20 00:49:39 +0530

    Replace Firebase config with placeholder values
    Updated Firebase configuration with placeholder values.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,14 @@
-]import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPZ1goaXwUxNATdTZ2vbsWYmfO2AGZ93A",
-  authDomain: "complaint-system-7d7f0.firebaseapp.com",
-  projectId: "complaint-system-7d7f0",
-  storageBucket: "complaint-system-7d7f0.firebasestorage.app",
-  messagingSenderId: "134478835602",
-  appId: "1:134478835602:web:31dac657fd1c2e09d89b4b"
+  apiKey: "PASTE_YOUR_REAL_KEY",
+  authDomain: "PASTE.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
